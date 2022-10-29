22c893fe2b8b77c40d2578ebabc273333a87a7ad
srivastavaharsh9888 <srivastavaharsh9888@gmail.com>
2022-10-29 09:29:06 +0530

    reformatted code and fixed queries

--- movie/urls.py
+++ movie/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import MovieDetail, ScoreManagement, LeadershipBoard
+from .views import MovieDetail, ScoreManagement, LeadershipBoard, UserCentricDashboard
 
 urlpatterns = [
     path('submit_score/', ScoreManagement.as_view(), name='submitscore'),
     path('get_leadership_board/', LeadershipBoard.as_view(), name='leadershipboard'),
-    path('get_usercentric_board/', ScoreManagement.as_view(), name='scoreboard'),
+    path('get_usercentric_board/', UserCentricDashboard.as_view(), name='scoreboard'),
     path('movies/', MovieDetail.as_view(), name='moviedetail'),
 ]
--- movie/views.py
+++ movie/views.py
@@ -11,10 +11,24 @@ class LeadershipBoard(generics.ListAPIView):
 
     def get_queryset(self):
         date_to_consider = self.request.query_params.get('date', datetime.today().strftime('%Y-%m-%d'))
-        return UserScore.objects.filter(score_date=date_to_consider).values('user__username').annotate(total_score=Sum('score')).order_by('-total_score')
+        return UserScore.objects.filter(score_date=date_to_consider).values('user__username').annotate(
+            total_score=Sum('score')).order_by('-total_score')
+
 
 class ScoreManagement(generics.CreateAPIView):
     serializer_class = ScoreManagementSerializer
+
+
+class UserCentricDashboard(generics.ListAPIView):
+    serializer_class = ScoreManagementSerializer
+    lookup_field = None
+
+    def get_queryset(self):
+        date_to_consider = self.request.query_params.get('date', datetime.today().strftime('%Y-%m-%d'))
+        username = self.request.query_params.get('username')
+        return UserScore.objects.filter(score_date=date_to_consider, user__username=username).values('user__username').annotate(
+            total_score=Sum('score')).order_by('-total_score')
+
 
 class MovieDetail(generics.ListAPIView):
     serializer_class = MovieSerializer
